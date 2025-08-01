0d994f844871cbf8066bf5f074147ed039421d02
Julissa <velazquezdalu@gmail.com>
2025-07-31 23:58:20 -0700

    connect BooksPage to local Database

--- NexusLibrarySystem/NexusLibrarySystem/Models/Book.cs
+++ NexusLibrarySystem/NexusLibrarySystem/Models/Book.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace NexusLibrarySystem.Models
 {
-    internal class Book
+    public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
     }
 }
+
--- NexusLibrarySystem/NexusLibrarySystem/Views/BooksPage.xaml.cs
+++ NexusLibrarySystem/NexusLibrarySystem/Views/BooksPage.xaml.cs
@@ -1,58 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using NexusLibrarySystem.Models;
 
 namespace NexusLibrarySystem.Views
 {
     public partial class BooksPage : Page
     {
-        private List<Book> mockBooks;
-
         public BooksPage()
         {
             InitializeComponent();
+            CargarLibros(); // Carga todos los libros al iniciar
+        }
 
-            // Datos simulados (sin base de datos)
-            mockBooks = new List<Book>
+        private void CargarLibros(string filtro = "")
+        {
+            try
             {
-                new Book { Id = 1, Title = "Cien años de soledad", Author = "Gabriel García Márquez" },
-                new Book { Id = 2, Title = "Don Quijote de la Mancha", Author = "Miguel de Cervantes" },
-                new Book { Id = 3, Title = "El Principito", Author = "Antoine de Saint-Exupéry" },
-                new Book { Id = 4, Title = "La sombra del viento", Author = "Carlos Ruiz Zafón" }
-            };
-
-            BooksGrid.ItemsSource = mockBooks;
+                List<Book> libros = Database.GetBooks(filtro);
+                BooksGrid.ItemsSource = libros;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar libros: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void Buscar_Click(object sender, RoutedEventArgs e)
         {
-            string query = SearchBox.Text.ToLower();
-
-            var resultado = mockBooks
-                .Where(b => b.Title.ToLower().Contains(query))
-                .ToList();
-
-            BooksGrid.ItemsSource = resultado;
+            string filtro = SearchBox.Text.Trim();
+            CargarLibros(filtro);
         }
-    }
-
-    // Clase Book temporal (puedes moverla a Models/Book.cs si ya existe)
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
     }
 }
